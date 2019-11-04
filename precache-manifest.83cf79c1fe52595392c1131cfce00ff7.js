60b9544c49a1082b83458cf2da6bdf83e660bd27
bakuzan <imannoyed4days@hotmail.co.uk>
2019-11-04 09:33:38 +0000

    Updates

---
+++ precache-manifest.83cf79c1fe52595392c1131cfce00ff7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa71bbc1e9731563c8f409043fc698d",
+    "url": "/sticky-fingers/404.html"
+  },
+  {
+    "revision": "f99c94680ea3cfc66ef1",
+    "url": "/sticky-fingers/css/app.8e577137.css"
+  },
+  {
+    "revision": "25bb04c242f3701202ef",
+    "url": "/sticky-fingers/css/history.7775ca3c.css"
+  },
+  {
+    "revision": "88cbccad2f70672edd7a22dcb569d738",
+    "url": "/sticky-fingers/img/favicon-16x16.png"
+  },
+  {
+    "revision": "e3d2b19e4b29d071e393de6d47f61ab4",
+    "url": "/sticky-fingers/img/favicon-32x32.png"
+  },
+  {
+    "revision": "1c6b29f28bfca40da77457e04ea2d468",
+    "url": "/sticky-fingers/img/icon-128x128.png"
+  },
+  {
+    "revision": "9a8575b892c49c2019dc82f52295e8bc",
+    "url": "/sticky-fingers/img/icon-192x192.png"
+  },
+  {
+    "revision": "497fe3a1fe7369051fb0fef107e2006b",
+    "url": "/sticky-fingers/img/icon-512x512.png"
+  },
+  {
+    "revision": "0d1b3d595f311b16aa28e3c582fc225d",
+    "url": "/sticky-fingers/index.html"
+  },
+  {
+    "revision": "f99c94680ea3cfc66ef1",
+    "url": "/sticky-fingers/js/app.87980d69.js"
+  },
+  {
+    "revision": "40580244d3def2b0c69a",
+    "url": "/sticky-fingers/js/chunk-vendors.70825d0b.js"
+  },
+  {
+    "revision": "25bb04c242f3701202ef",
+    "url": "/sticky-fingers/js/history.564b11df.js"
+  },
+  {
+    "revision": "e6d44ad62bc4f307d736006ec81f7729",
+    "url": "/sticky-fingers/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/sticky-fingers/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sticky-fingers/precache-manifest.8685e878131b5efc90329f31cb4cfb39.js"
+  "/sticky-fingers/precache-manifest.83cf79c1fe52595392c1131cfce00ff7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sticky-fingers"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "sticky-fingers"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
